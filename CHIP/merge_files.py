20b3266e5968391f8ea74c8f50278cc5b3c9a62e
mathewslr <mathewslr@nih.gov>
2020-08-23 17:23:15 -0400

    dictionary created (not ordered)

---
+++ CHIP/merge_files.py
@@ -0,0 +1,17 @@
+import os
+import sys
+
+arr = os.listdir('sample_data')
+
+master_map = {}
+for i in arr:
+    x = i.split('_')
+    sample_num = x[2]
+    run_num = x[4]
+    if (sample_num,run_num) in master_map:
+        master_map[(sample_num,run_num)].append(i)
+    else:
+        master_map[(sample_num,run_num)] = []
+        master_map[(sample_num,run_num)].append(i)
+
+print(master_map)
